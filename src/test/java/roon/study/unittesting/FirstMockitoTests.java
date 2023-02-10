20f7669dbd9c0cf43dfabe782c7acd6e23e616fc
roon <think-roon@naver.com>
2023-02-10 23:00:56 +0900

    [Mockito Docs] 첫번째 mock, stubbing을 이용한 단위 테스트

--- src/test/java/roon/study/unittesting/FirstMockitoTests.java
+++ src/test/java/roon/study/unittesting/FirstMockitoTests.java
@@ -1,15 +1,17 @@
 package roon.study.unittesting;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.verification.VerificationModeFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class FirstMockitoTests {
+
     @Test
-    public void first_mock_test(){
+    public void first_mock_test() {
         // create mock
         List<String> mockList = mock(List.class);
 
@@ -22,6 +24,33 @@ public class FirstMockitoTests {
         verify(mockList).add("one");
         // verify(mockList, VerificationModeFactory.atLeast(2)).add("one");
         verify(mockList).clear();
+    }
+
+    @Test
+    public void first_mock_test_with_stubbing() {
+        // create mock
+        List<String> mockedList = mock(ArrayList.class);
+
+        // stubbing
+        // 가정하기라 보면 될듯? (pre-programmed)
+        when(mockedList.get(0)).thenReturn("first(stubbed)");
+        when(mockedList.get(1)).thenThrow(new IndexOutOfBoundsException("no second item"));
+
+        System.out.println(mockedList.get(0));
+        System.out.println(mockedList.get(1));
+        System.out.println(mockedList.get(2));
+
+        verify(mockedList).get(0);
+    }
+
+    @Test
+    public void test_argument_matchers() {
+        List<String> mockedList = mock(ArrayList.class);
+
+        System.out.println(anyInt());
+        when(mockedList.get(anyInt())).thenReturn("item");
+
+        assertThat(mockedList.get(0)).isEqualTo("item");
     }
 
 }
