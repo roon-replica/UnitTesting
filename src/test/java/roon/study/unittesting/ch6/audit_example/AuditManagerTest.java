aa7d1789fbd28fff84b4e61166ee414d0f65ef7a
roon <think-roon@naver.com>
2023-03-05 12:31:22 +0900

    [UnitTesting-6장-단위 테스트 스타일] 파일 시스템을 인터페이스로 분리 - mock을 사용하여 테스트 개선!

--- src/test/java/roon/study/unittesting/ch6/audit_example/AuditManagerTest.java
+++ src/test/java/roon/study/unittesting/ch6/audit_example/AuditManagerTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.*;
 
 //@SpringBootTest
 class AuditManagerTest {
@@ -37,6 +41,30 @@ class AuditManagerTest {
 
         System.out.println("a directory and a file are deleted after create.");
         System.out.println(deletedFilePaths);
+    }
+
+    @Test
+    public void A_new_file_is_created_when_the_current_file_overflows() throws IOException {
+        Path directoryPath = Path.of("./audit_results");
+        var fileSystemMock = mock(FileInterface.class);
+        when(fileSystemMock.getAllFilePaths(directoryPath)).thenReturn(
+                Stream.of(
+                        Path.of("./audit_results/audit_1.txt"),
+                        Path.of("./audit_results/audit_2.txt")
+                )
+        );
+
+        final int maxLinesPerFile = 3;
+        Path lastFilePath = Path.of("./audit_results/audit_2.txt");
+        when(fileSystemMock.getLineCount(lastFilePath)).thenReturn(maxLinesPerFile);
+
+        AuditManager sut = new AuditManager(maxLinesPerFile, "audit_results", fileSystemMock);
+        String visitorName = "roon";
+        LocalDateTime now = LocalDateTime.now();
+        sut.addRecord(visitorName, now);
+
+        Path nextFilePath = Path.of("./audit_results/audit_3.txt");
+        verify(fileSystemMock).writeToFile(nextFilePath, visitorName + " " + now);
     }
 
 }
